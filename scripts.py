cab3d85ad980a985699ac7b5ff0b455a8c5a8050
AdaSawilska <84930128+AdaSawilska@users.noreply.github.com>
2022-05-15 16:10:26 +0200

    labels

--- scripts.py
+++ scripts.py
@@ -5,59 +5,98 @@ import h5py
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import pydicom as pydicom
 from PIL import Image
 from natsort import natsorted
 
 # Creates new column with two classes 0-normal, 1-pathologic
+from scipy.io import loadmat
+
+
 def newClass():
-    data = pd.read_csv('./labels.txt', sep=" ", header=None)
-    data.columns = ["name", "1", "2", "3", "4", "5", "6"]
-    data["7"] = np.where(data['2'] != 'NORM', 1, 0)
-
-    data.to_csv('labels.csv', index=False)
-    print("done")
-
-# Reads photos and save them as array in .h5 file
-def readBreastImages():
-    path = './input/output_stretch'
-    img = []
+    path = './dataset'
     list_of_images = natsorted(os.listdir(path))
-    for i, image in enumerate(list_of_images):
-        with open(path+f'/{image}', 'rb') as pgmf:
-            im = Image.open(pgmf)
-            #img.append(np.asarray(im.resize((256, 256))))
-            img.append(np.asarray(im))
-        #plt.imshow(img[i])
-        #plt.show()
-    img = np.asarray(img)
-    h5f = h5py.File('images_stretch.h5', 'w')
-    h5f.create_dataset('images', data=img)
-    h5f.close()
-    print('done')
+    choices = ["norm", "arch", "spicule"]
 
-def savetoCSV():
-    results = {"Basia": 10, "Ada": 18, "Jola": 23, "Kasia": 3}
+    with open('ROI_labels.csv', 'w', newline='') as csvfile:
+        header_key = ['ROI', 'Class']
+        new_val = csv.DictWriter(csvfile, fieldnames=header_key)
+        new_val.writeheader()
+        for img_name in list_of_images:
+            conditions = ['norm' in img_name, 'arch' in img_name, 'spicule' in img_name]
+            new_val.writerow({header_key[0]: img_name, header_key[1]: np.select(conditions, choices, default=np.nan)})
 
-    # save to empty file
-    # with open('try.csv', 'w', newline='') as csvfile:
-    #     header_key = ['Imie', 'Wiek']
+
+    # data = pd.read_csv('./ROI_labels.csv', sep=" ", header=)
+    # data.columns = ["ROI"]
+    # data["Class"] = np.where('norm' in data["ROI"])
+    #
+    # data.to_csv('labels.csv', index=False)
+
+    # with open('ROI_labels.csv', 'w', newline='') as csvfile:
+    #     header_key = ['ROI', 'Class']
     #     new_val = csv.DictWriter(csvfile, fieldnames=header_key)
     #     new_val.writeheader()
     #     for new_k in results:
     #         new_val.writerow({header_key[0]: new_k, header_key[1]: results[new_k]})
 
-    # add column to existing csv file
-    df = pd.read_csv('try.csv')
-    df["Waga"]=[34, 56, 63, 10]
 
-    print("read")
+
+
+
+#
+# Reads photos and save them as array in .h5 file
+def readBreastImages():
+    path = './output_strech'
+    img = []
+    list_of_images = natsorted(os.listdir(path))
+    for i, image in enumerate(list_of_images):
+        with open(path+f'/{image}', 'rb') as pgmf:
+            #im = Image.open(pgmf)
+            im = pydicom.dcmread(pgmf).pixel_array
+            # plt.imshow(im, cmap=plt.cm.bone)
+            # plt.show()
+            img.append(np.asarray(im))
+        #plt.imshow(img[i])
+        #plt.show()
+    img = np.asarray(img)
+    h5f = h5py.File('ROIstrech.h5', 'w')
+    h5f.create_dataset('images', data=img)
+    h5f.close()
+    print('done')
+#
+# def savetoCSV():
+#     results = {"Basia": 10, "Ada": 18, "Jola": 23, "Kasia": 3}
+#
+#     # save to empty file
+#     # with open('try.csv', 'w', newline='') as csvfile:
+#     #     header_key = ['Imie', 'Wiek']
+#     #     new_val = csv.DictWriter(csvfile, fieldnames=header_key)
+#     #     new_val.writeheader()
+#     #     for new_k in results:
+#     #         new_val.writerow({header_key[0]: new_k, header_key[1]: results[new_k]})
+#
+#     # add column to existing csv file
+#     df = pd.read_csv('try.csv')
+#     df["Waga"]=[34, 56, 63, 10]
+#
+#     print("read")
+#
+def readDescriptors():
+    descriptor = loadmat("./input/HOG_wyniki.mat")
+    h5f = h5py.File('hog_original.h5', 'w')
+    h5f.create_dataset('images', data=descriptor["J"])
+    h5f.close()
+    print("done")
+
 
 
 
 if __name__ == '__main__':
     #newClass()
-    readBreastImages()
+    #readBreastImages()
     #savetoCSV()
+    readDescriptors()
 
 
 

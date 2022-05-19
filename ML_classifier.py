0fbfd305002123e1b20a0a0d02cc357840f09ad0
AdaSawilska <84930128+AdaSawilska@users.noreply.github.com>
2022-05-19 23:44:34 +0200

    beautiful code

--- ML_classifier.py
+++ ML_classifier.py
@@ -1,10 +1,11 @@
-
+from os.path import exists
 
 import h5py
 import numpy as np
 import pandas as pd
 import csv
 import matplotlib.pyplot as plt
+from sklearn import svm
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
@@ -14,112 +15,106 @@ from sklearn.neighbors import KNeighborsClassifier
 from xgboost import XGBClassifier
 from tpot import TPOTClassifier
 
-filename = 'images_reduce.h5'
+filenames = ['hog_2.h5']
+for name in filenames:
 
-h5f = h5py.File(filename,'r')
-img = h5f['images'][:][:, ::10, ::10]   #changing these parameters cause change of image size
-h5f.close()
-#h5f1.close()
+    h5f = h5py.File(name, 'r')
+    # jeśli klasyfikujemy wyjscie deskryptorów np. hog
+    descriptors = h5f['images'][:][:]
+    scan_lr_flat = descriptors.reshape((descriptors.shape[0], -1))
+    idx_vec = np.arange(descriptors.shape[0])
 
+    # jeśli klasyfikujemy całe obrazy np. bsif
+    # img = h5f['images'][:][:, :, :]
+    # plt.imshow(img[8])
+    # plt.show()
+    #scan_lr_flat = img.reshape((img.shape[0], -1))
+    #idx_vec = np.arange(scan_lr_flat.shape[0])
+    h5f.close()
 
+    labels = pd.read_csv("ROI_labels.csv")
+    class_info = labels["Class"].values
+    class_le = LabelEncoder()
+    class_le.fit(class_info)
+    class_vec = class_le.transform(class_info)
 
+    # podział danych na testowe i treningowe: jeśli wkładamy deskryptor, to jako pierwszy argument wpisać "descriptors"
+    # jeśli obrazy, to wpisać "scan_lr_flat"
+    x_train, x_test, y_train, y_test, idx_train, idx_test = train_test_split(descriptors, #scan_lr_flat,
+                                                                             class_vec,
+                                                                             idx_vec,
+                                                                             random_state=2017,
+                                                                             test_size=0.5,
+                                                                             stratify=class_vec)
+    print('Training', x_train.shape)
+    print('Testing', x_test.shape)
 
-data = pd.read_csv("ROI_labels.csv")
-class_info = data["Class"].values
-#plt.imshow(img[8])
-#plt.show()
-#
-#scan_lr_flat = img.reshape((img.shape[0], -1))
-#scan_lr_flat = descriptors.reshape((descriptors.shape[0], -1))
-class_le = LabelEncoder()
-class_le.fit(class_info)
-class_vec = class_le.transform(class_info)
+    creport = lambda gt_vec, pred_vec: classification_report(gt_vec, pred_vec, target_names=["0", "1"])
 
-#idx_vec = np.arange(scan_lr_flat.shape[0])
-idx_vec = np.arange(descriptors.shape[0])
-x_train, x_test, y_train, y_test, idx_train, idx_test = train_test_split(descriptors, #scan_lr_flat,
-                                                                         class_vec,
-                                                                         idx_vec,
-                                                                         random_state=2017,
-                                                                         test_size=0.5,
-                                                                         stratify=class_vec)
-print('Training', x_train.shape)
-print('Testing', x_test.shape)
+    #Most Frequent Model
+    dc = DummyClassifier(strategy='most_frequent')
+    dc.fit(x_train, y_train)
+    y_pred = dc.predict(x_test)
+    print('Accuracy %2.2f%%' % (100 * accuracy_score(y_test, y_pred)))
+    results = {"DummyClassifier": round(100 * accuracy_score(y_test, y_pred),2)}
+    print(creport(y_test, y_pred))
+    #
+    # # KNearestNeighbors
+    # knn = KNeighborsClassifier(8)
+    # knn.fit(x_train, y_train)
+    # y_pred = knn.predict(x_test)
+    # print('Accuracy %2.2f%%' % (100 * accuracy_score(y_test, y_pred)))
+    # results["kNN"] = round(100 * accuracy_score(y_test, y_pred),2)
+    # print(creport(y_test, y_pred))
+    #
+    # # Random Forest
+    # rfc = RandomForestClassifier()
+    # rfc.fit(x_train, y_train)
+    # y_pred = rfc.predict(x_test)
+    # print('Accuracy %2.2f%%' % (100 * accuracy_score(y_test, y_pred)))
+    # results["RandomForest"] = round(100 * accuracy_score(y_test, y_pred),2)
+    # print(creport(y_test, y_pred))
 
-creport = lambda gt_vec, pred_vec: classification_report(gt_vec, pred_vec, target_names=["norm", "arch", "spicule"])
-                                                         # target_names=[x.decode() for x in
-                                                         #               class_le.classes_])
-# Most Frequent Model
-dc = DummyClassifier(strategy='most_frequent')
-dc.fit(x_train, y_train)
-y_pred = dc.predict(x_test)
-print('Accuracy %2.2f%%' % (100 * accuracy_score(y_test, y_pred)))
-results = {"DummyClassifier": round(100 * accuracy_score(y_test, y_pred),2)}
-print(creport(y_test, y_pred))
+    # Gradient Boosting
+    # xgc = XGBClassifier(silent=False, nthread=2)
+    # xgc.fit(x_train, y_train)
+    # y_pred = xgc.predict(x_test)
+    # print('Accuracy %2.2f%%' % (100 * accuracy_score(y_test, y_pred)))
+    # results["XGB"] = round(100 * accuracy_score(y_test, y_pred),2)
+    # print(creport(y_test, y_pred))
 
-# KNearestNeighbors
-knn = KNeighborsClassifier(8)
-knn.fit(x_train, y_train)
-y_pred = knn.predict(x_test)
-print('Accuracy %2.2f%%' % (100 * accuracy_score(y_test, y_pred)))
-results["kNN"] = round(100 * accuracy_score(y_test, y_pred),2)
-print(creport(y_test, y_pred))
+    # SVM
+    svm = svm.SVC(kernel='linear') #kernel='rbf'
+    svm.fit(x_train, y_train)
+    y_pred = svm.predict(x_test)
+    print('Accuracy %2.2f%%' % (100 * accuracy_score(y_test, y_pred)))
+    results["SVM"] = round(100 * accuracy_score(y_test, y_pred), 2)
+    print(creport(y_test, y_pred))
 
-# Random Forest
-rfc = RandomForestClassifier()
-rfc.fit(x_train, y_train)
-y_pred = rfc.predict(x_test)
-print('Accuracy %2.2f%%' % (100 * accuracy_score(y_test, y_pred)))
-results["RandomForest"] = round(100 * accuracy_score(y_test, y_pred),2)
-print(creport(y_test, y_pred))
+    # AutoML
+    # tpc = TPOTClassifier(generations=2, population_size=5, verbosity=True)
+    # tpc.fit(x_train, y_train)
+    # y_pred = tpc.predict(x_test)
+    # print('Accuracy %2.2f%%' % (100 * accuracy_score(y_test, y_pred)))
+    # #results["TPOT"] = round(100 * accuracy_score(y_test, y_pred),2)
+    # print(creport(y_test, y_pred))
 
-# Gradient Boosting
-xgc = XGBClassifier(silent=False, nthread=2)
-xgc.fit(x_train, y_train)
-y_pred = xgc.predict(x_test)
-print('Accuracy %2.2f%%' % (100 * accuracy_score(y_test, y_pred)))
-results["XGB"] = round(100 * accuracy_score(y_test, y_pred),2)
-print(creport(y_test, y_pred))
-
-# AutoML
-
-tpc = TPOTClassifier(generations=2, population_size=5, verbosity=True)
-tpc.fit(x_train, y_train)
-y_pred = tpc.predict(x_test)
-print('Accuracy %2.2f%%' % (100 * accuracy_score(y_test, y_pred)))
-results["TPOT"] = round(100 * accuracy_score(y_test, y_pred),2)
-print(creport(y_test, y_pred))
-
-# images
-csvname = 'results_roi.csv'
-if filename == 'ROI.h5':
-    with open(csvname, 'w', newline='') as csvfile:
-        header_key = ['Classifier', 'Accuracy for original mammograms']
-        new_val = csv.DictWriter(csvfile, fieldnames=header_key)
-        new_val.writeheader()
-        for new_k in results:
-            new_val.writerow({header_key[0]: new_k, header_key[1]: results[new_k]})
-else:
-    s = filename.split("_")
-    #s = s[1].split(".")
-    df = pd.read_csv(csvname)
-    df[f"Accuracy {s[0]}"] = [results["DummyClassifier"], results["kNN"], results["RandomForest"], results["XGB"], results["TPOT"] ]
-    df.to_csv(csvname, index=False)
-
-# descriptors
-# if filename == 'hog.h5':
-#     with open('results_descriptors.csv', 'w', newline='') as csvfile:
-#         header_key = ['Classifier', 'Accuracy for HOG']
-#         new_val = csv.DictWriter(csvfile, fieldnames=header_key)
-#         new_val.writeheader()
-#         for new_k in results:
-#             new_val.writerow({header_key[0]: new_k, header_key[1]: results[new_k]})
-# else:
-#     s = filename.split(".")
-#     df = pd.read_csv('results_descriptors.csv')
-#     df[f"Accuracy {s[0]}"] = [results["DummyClassifier"], results["kNN"], results["RandomForest"], results["XGB"], results["TPOT"] ]
-#     df.to_csv('results_descriptors.csv', index=False)
+    # zapis wyników do pliku CSV
+    path_to_results = 'results_descriptors.csv'
+    if exists(path_to_results)==False:
+        with open(path_to_results, 'w', newline='') as csvfile:
+            header_key = ['Classifier', 'Accuracy for HOG']
+            new_val = csv.DictWriter(csvfile, fieldnames=header_key)
+            new_val.writeheader()
+            for new_k in results:
+                new_val.writerow({header_key[0]: new_k, header_key[1]: results[new_k]})
+    else:
+        s = name.split(".")
+        df = pd.read_csv(path_to_results)
+        # jeśli któregoś z klasyfikatorów nie używamy to trzeba wywalić i dać np. jakiś string albo 0
+        df[f"Accuracy {s[0]}"] = [results["DummyClassifier"], results["kNN"], results["RandomForest"], results["XGB"], "nic" ]
+        df.to_csv(path_to_results, index=False)
 
 
-print("done")
+    print("done")
 
--- scripts.py
+++ scripts.py
@@ -6,97 +6,65 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import pydicom as pydicom
+import scipy
 from PIL import Image
 from natsort import natsorted
-
+import cv2
 # Creates new column with two classes 0-normal, 1-pathologic
 from scipy.io import loadmat
 
 
-def newClass():
+def labelCreator():
     path = './dataset'
     list_of_images = natsorted(os.listdir(path))
-    choices = ["norm", "arch", "spicule"]
-
     with open('ROI_labels.csv', 'w', newline='') as csvfile:
         header_key = ['ROI', 'Class']
         new_val = csv.DictWriter(csvfile, fieldnames=header_key)
         new_val.writeheader()
         for img_name in list_of_images:
-            conditions = ['norm' in img_name, 'arch' in img_name, 'spicule' in img_name]
-            new_val.writerow({header_key[0]: img_name, header_key[1]: np.select(conditions, choices, default=np.nan)})
-
-
-    # data = pd.read_csv('./ROI_labels.csv', sep=" ", header=)
-    # data.columns = ["ROI"]
-    # data["Class"] = np.where('norm' in data["ROI"])
-    #
-    # data.to_csv('labels.csv', index=False)
-
-    # with open('ROI_labels.csv', 'w', newline='') as csvfile:
-    #     header_key = ['ROI', 'Class']
-    #     new_val = csv.DictWriter(csvfile, fieldnames=header_key)
-    #     new_val.writeheader()
-    #     for new_k in results:
-    #         new_val.writerow({header_key[0]: new_k, header_key[1]: results[new_k]})
+            new_val.writerow({header_key[0]: img_name, header_key[1]: np.where('norm' in img_name, 0, 1)})
 
 
 
-
-
-#
-# Reads photos and save them as array in .h5 file
+# Przekształca zdjęcia bsifem
 def readBreastImages():
-    path = './output_strech'
+    path = './dataset'
     img = []
     list_of_images = natsorted(os.listdir(path))
     for i, image in enumerate(list_of_images):
         with open(path+f'/{image}', 'rb') as pgmf:
-            #im = Image.open(pgmf)
             im = pydicom.dcmread(pgmf).pixel_array
-            # plt.imshow(im, cmap=plt.cm.bone)
-            # plt.show()
+            im = filtersBSIF(im)
             img.append(np.asarray(im))
-        #plt.imshow(img[i])
-        #plt.show()
     img = np.asarray(img)
-    h5f = h5py.File('ROIstrech.h5', 'w')
+    h5f = h5py.File('ROIbsif11_11_5.h5', 'w')
     h5f.create_dataset('images', data=img)
     h5f.close()
     print('done')
-#
-# def savetoCSV():
-#     results = {"Basia": 10, "Ada": 18, "Jola": 23, "Kasia": 3}
-#
-#     # save to empty file
-#     # with open('try.csv', 'w', newline='') as csvfile:
-#     #     header_key = ['Imie', 'Wiek']
-#     #     new_val = csv.DictWriter(csvfile, fieldnames=header_key)
-#     #     new_val.writeheader()
-#     #     for new_k in results:
-#     #         new_val.writerow({header_key[0]: new_k, header_key[1]: results[new_k]})
-#
-#     # add column to existing csv file
-#     df = pd.read_csv('try.csv')
-#     df["Waga"]=[34, 56, 63, 10]
-#
-#     print("read")
-#
+
+# zapisanie całej macierzy deskryptora do pliku h5
 def readDescriptors():
-    descriptor = loadmat("./input/HOG_wyniki.mat")
-    h5f = h5py.File('hog_original.h5', 'w')
+    descriptor = loadmat("./input/HOG11.mat")   # tu wpisujemy ścieżkę do macierzy
+    h5f = h5py.File('hog_11.h5', 'w')           # tu wpisujemy nazwę jaką ma mieć plik h5
     h5f.create_dataset('images', data=descriptor["J"])
     h5f.close()
     print("done")
 
 
-
+# nałożenie BSIF'u na obraz
+def filtersBSIF(image):
+    mat1 = scipy.io.loadmat('texturefilters/ICAtextureFilters_11x11_5bit.mat')
+    kernel1 = np.ascontiguousarray(mat1['ICAtextureFilters'].T)
+    filtered=cv2.filter2D(image, cv2.CV_64F, kernel1[4])
+    ret, bin_image = cv2.threshold(filtered, 0, 255, cv2.THRESH_BINARY)
+    return bin_image
 
 if __name__ == '__main__':
-    #newClass()
+    #labelCreator()
     #readBreastImages()
     #savetoCSV()
     readDescriptors()
+    #readMSER()
 
 
 
